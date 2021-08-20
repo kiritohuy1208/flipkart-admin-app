f7c9c5f379f0bd39d2a478e085d5801042ef25ed
Tô Huy <kiritohuy1208>
2021-08-20 15:20:16 +0700

    update urlConfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,7 @@
-export const api = 'http://localhost:8080/api'
-export const generatePublicUrl = (filename) =>{
-    return `http://localhost:8080/public/${filename}`
-}
+const baseUrl =
+  "http://localhost:8080" || "https://flipkart-rest-backend.herokuapp.com";
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (filename) => {
+  return `${baseUrl}/public/${filename}`;
+};
