eb5ed40be75d96dba27e98a188897e8fd50b7d05
Tô Quang Huy <18520856@gm.uit.edu.vn>
2022-01-24 14:50:50 +0700

    update config

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://flipkart-rest-backend.herokuapp.com";
-
+//const baseUrl = "http://localhost:8080";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (filename) => {
   return `${baseUrl}/public/${filename}`;
